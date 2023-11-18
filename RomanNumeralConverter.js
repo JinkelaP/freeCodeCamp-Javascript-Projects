323939c59b2aaff9c53eca44ab6f3f8946c19bdb
Haochen Zhu <73946565+JinkelaP@users.noreply.github.com>
2023-11-18 14:25:21 +1300

    Update RomanNumeralConverter.js

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -1,5 +1,15 @@
 function convertToRoman(num) {
-    return num;
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let romanNum = '';
+    for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+            romanNum += syms[i];
+            num -= val[i];
+        }
+    }
+    return romanNum;
    }
    
    convertToRoman(36);
